7f2fa65e743e9f30645b4ae026e9a0e750466a15
guilhem <guilhem@alteragile.com>
2020-06-13 11:50:59 +0200

    sums ok #22

--- src/Controller/AnalyticsController.php
+++ src/Controller/AnalyticsController.php
@@ -7,6 +7,7 @@ use App\Entity\Task;
 use App\Entity\Time;
 use App\Entity\User;
 use App\Form\AnalyticsType;
+use App\Service\AnalyticsServices;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,7 +20,7 @@ class AnalyticsController extends AbstractController
      * @Route(path="/analytics", name="analytics", methods={"GET", "POST"})
      * @IsGranted("ROLE_USER")
      */
-    public function index(Request $request, EntityManagerInterface $manager)
+    public function index(Request $request, EntityManagerInterface $manager, AnalyticsServices $analyticsServices)
     {
         $form = $this->createForm(AnalyticsType::class, null, [
             'projects' => $manager->getRepository(Project::class)->findAll(),
@@ -36,73 +37,15 @@ class AnalyticsController extends AbstractController
             // We get all times according to data form
             $times = $manager->getRepository(Time::class)->getTimes($projectsData, $tasksData, $usersData, $form['startTime']->getData(), $form['endTime']->getData());
 
-            // the total of hours for each projects passed in data form
-            $projects = [];
-            /** @var Project $project */
-            foreach ($projectsData as $project) {
-                $projects[$project->getId()]['name'] = $project->getName();
-                /** @var Task $task */
-                foreach ($tasksData as $task) {
-                    $projects[$project->getId()]['tasks'][$task->getId()]['name'] = $task->getName();
-                    $projects[$project->getId()]['tasks'][$task->getId()]['total'] = 0;
-                    /** @var Time $time */
-                    foreach ($times as $time) {
-                        if ($project === $time->getProject() && $task === $time->getTask()) {
-                            $diff = $time->getEndTime()->diff($time->getStartTime());
-                            $hours1 = $hours2 = $hours3 = 0;
-                            if ($diff->format('%a') > 0) {
-                                $hours1 = $diff->format('%a') * 24;
-                            }
-                            if ($diff->format('%h') > 0) {
-                                $hours2 = $diff->format('%h');
-                            }
-                            if ($diff->format('%m') > 0) {
-                                $hours3 = round($diff->format('%m') / 60, 2);
-                            }
-
-                            $time->diff = $hours1 + $hours2 + $hours3;
-                            $projects[$project->getId()]['tasks'][$task->getId()]['total'] = $time->diff + $projects[$project->getId()]['tasks'][$task->getId()]['total'];
-                        }
-                    }
-                }
-            }
-
-            // the total of hours for each projects passed in data form
-            $tasks = [];
-            /** @var Task $task */
-            foreach ($tasksData as $task) {
-                $tasks[$task->getId()]['name'] = $task->getName();
-                /** @var Project $project */
-                foreach ($projectsData as $project) {
-                    $tasks[$task->getId()]['projects'][$project->getId()]['name'] = $project->getName();
-                    $tasks[$task->getId()]['projects'][$project->getId()]['total'] = 0;
-                    /** @var Time $time */
-                    foreach ($times as $time) {
-                        if ($project === $time->getProject() && $task === $time->getTask()) {
-                            $diff = $time->getEndTime()->diff($time->getStartTime());
-                            $hours1 = $hours2 = $hours3 = 0;
-                            if ($diff->format('%a') > 0) {
-                                $hours1 = $diff->format('%a') * 24;
-                            }
-                            if ($diff->format('%h') > 0) {
-                                $hours2 = $diff->format('%h');
-                            }
-                            if ($diff->format('%m') > 0) {
-                                $hours3 = round($diff->format('%m') / 60, 2);
-                            }
-
-                            $time->diff = $hours1 + $hours2 + $hours3;
-                            $tasks[$task->getId()]['projects'][$project->getId()]['total'] = $time->diff + $tasks[$task->getId()]['projects'][$project->getId()]['total'];
-                        }
-                    }
-                }
-                dump($tasks);
-            }
+            $projects = $analyticsServices->analyzePerProjects($projectsData, $tasksData, $usersData, $times);
+            $tasks = $analyticsServices->analyzePerTasks($projectsData, $tasksData, $usersData, $times);
+            $users = $analyticsServices->analyzePerUsers($projectsData, $tasksData, $usersData, $times);
 
             return $this->render('analytics/results.html.twig', [
                 'times' => $times,
                 'projects' => $projects,
-                'tasks' => $tasks
+                'tasks' => $tasks,
+                'users' => $users
             ]);
         }
 
--- src/Service/AnalyticsServices.php
+++ src/Service/AnalyticsServices.php
@@ -4,10 +4,137 @@
 namespace App\Service;
 
 
+use App\Entity\Project;
+use App\Entity\Task;
+use App\Entity\Time;
+use App\Entity\User;
+
 class AnalyticsServices
 {
-    public function getTimes()
+    public function analyzePerProjects(array $projects, array $tasks, array $users, array $times): array
     {
+        $stats = [];
 
+
+        /** @var Project $project */
+        foreach ($projects as $project) {
+            $stats[$project->getId()]['name'] = $project->getName();
+
+            // Per projects and tasks
+            /** @var Task $task */
+            foreach ($tasks as $task) {
+                $stats[$project->getId()]['tasks'][$task->getId()]['name'] = $task->getName();
+                $stats[$project->getId()]['tasks'][$task->getId()]['total'] = 0;
+                /** @var Time $time */
+                foreach ($times as $time) {
+                    if ($project === $time->getProject() && $task === $time->getTask()) {
+                        $stats[$project->getId()]['tasks'][$task->getId()]['total'] = $this->timeToHours($time) + $stats[$project->getId()]['tasks'][$task->getId()]['total'];
+                    }
+                }
+            }
+
+            // Per projects and users
+            /** @var User $user */
+            foreach ($users as $user) {
+                $stats[$project->getId()]['users'][$user->getId()]['username'] = $user->getUsername();
+                $stats[$project->getId()]['users'][$user->getId()]['total'] = 0;
+                foreach ($times as $time) {
+                    if ($project === $time->getProject() && $user === $time->getUser()) {
+                        $stats[$project->getId()]['users'][$user->getId()]['total'] = $this->timeToHours($time) + $stats[$project->getId()]['users'][$user->getId()]['total'];
+                    }
+                }
+            }
+        }
+
+        return $stats;
+    }
+
+    public function analyzePerTasks(array $projects, array $tasks, array $users, array $times): array
+    {
+        $stats = [];
+
+        /** @var Task $task */
+        foreach ($tasks as $task) {
+            $stats[$task->getId()]['name'] = $task->getName();
+
+            // Per tasks and projects
+            /** @var Project $project */
+            foreach ($projects as $project) {
+                $stats[$task->getId()]['projects'][$project->getId()]['name'] = $project->getName();
+                $stats[$task->getId()]['projects'][$project->getId()]['total'] = 0;
+                /** @var Time $time */
+                foreach ($times as $time) {
+                    if ($project === $time->getProject() && $task === $time->getTask()) {
+                        $stats[$task->getId()]['projects'][$project->getId()]['total'] = $this->timeToHours($time) + $stats[$task->getId()]['projects'][$project->getId()]['total'];
+                    }
+                }
+            }
+
+            /** @var User $user */
+            foreach ($users as $user) {
+                $stats[$task->getId()]['users'][$user->getId()]['username'] = $user->getUsername();
+                $stats[$task->getId()]['users'][$user->getId()]['total'] = 0;
+                foreach ($times as $time) {
+                    if ($task === $time->getTask() && $user === $time->getUser()) {
+                        $stats[$task->getId()]['users'][$user->getId()]['total'] = $this->timeToHours($time) + $stats[$task->getId()]['users'][$user->getId()]['total'];
+                    }
+                }
+            }
+        }
+
+        return $stats;
+    }
+
+    public function analyzePerUsers(array $projects, array $tasks, array $users, array $times): array
+    {
+        $stats = [];
+
+        /** @var User $user */
+        foreach ($users as $user) {
+            $stats[$user->getId()]['username'] = $user->getUsername();
+
+            // Per users and projects
+            /** @var Project $project */
+            foreach ($projects as $project) {
+                $stats[$user->getId()]['projects'][$project->getId()]['name'] = $project->getName();
+                $stats[$user->getId()]['projects'][$project->getId()]['total'] = 0;
+                /** @var Time $time */
+                foreach ($times as $time) {
+                    if ($project === $time->getProject() && $user === $time->getUser()) {
+                        $stats[$user->getId()]['projects'][$project->getId()]['total'] = $this->timeToHours($time) + $stats[$user->getId()]['projects'][$project->getId()]['total'];
+                    }
+                }
+            }
+
+            /** @var Task $task */
+            foreach ($tasks as $task) {
+                $stats[$user->getId()]['tasks'][$task->getId()]['name'] = $task->getName();
+                $stats[$user->getId()]['tasks'][$task->getId()]['total'] = 0;
+                foreach ($times as $time) {
+                    if ($task === $time->getTask() && $user === $time->getUser()) {
+                        $stats[$user->getId()]['tasks'][$task->getId()]['total'] = $this->timeToHours($time) + $stats[$user->getId()]['tasks'][$task->getId()]['total'];
+                    }
+                }
+            }
+        }
+
+        return $stats;
+    }
+
+    public function timeToHours(Time $time): float
+    {
+        $diff = $time->getEndTime()->diff($time->getStartTime());
+        $hours1 = $hours2 = $hours3 = 0;
+        if ($diff->format('%a') > 0) {
+            $hours1 = $diff->format('%a') * 24;
+        }
+        if ($diff->format('%h') > 0) {
+            $hours2 = $diff->format('%h');
+        }
+        if ($diff->format('%m') > 0) {
+            $hours3 = round($diff->format('%m') / 60, 2);
+        }
+
+        return $hours1 + $hours2 + $hours3;
     }
 }
