58687edb8bcff1206daf7b1f6e24d0e220d19478
ericspichiger <e.spichiger@hiveweb.ch>
2020-06-15 19:14:40 +0200

    [UI/UX] Add template vali-admin

--- assets/js/analytics-form.js
+++ assets/js/analytics-form.js
@@ -2,14 +2,14 @@ require('../css/analytics-form.scss');
 var $ = require('jquery');
 
 $(document).ready(function () {
-    $(document).on('click', '#appbundle_analytics_forever', function () {
-        console.log('coucou')
-        if ($(this).is(':checked')) {
-            $('#appbundle_analytics_startTime').attr('disabled', true);
-            $('#appbundle_analytics_endTime').attr('disabled', true);
-        } else {
-            $('#appbundle_analytics_startTime').attr('disabled', false);
-            $('#appbundle_analytics_endTime').attr('disabled', false);
-        }
-    })
+  $(document).on('click', '#appbundle_analytics_forever', function () {
+    console.log('coucou')
+    if ($(this).is(':checked')) {
+      $('#appbundle_analytics_startTime').attr('disabled', true);
+      $('#appbundle_analytics_endTime').attr('disabled', true);
+    } else {
+      $('#appbundle_analytics_startTime').attr('disabled', false);
+      $('#appbundle_analytics_endTime').attr('disabled', false);
+    }
+  })
 })
