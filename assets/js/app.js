883cdb95f2ca1f37a3fbe149371e71efe79dbb87
ericspichiger <e.spichiger@hiveweb.ch>
2020-06-15 19:14:40 +0200

    [UI/UX] Add template vali-admin

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,4 @@
-require('../css/app.scss');
+import '../scss/app.scss';
 import $ from 'jquery';
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
-
-$( document ).ready(function() {
-    var $accountType = $('#registration_accountType');
-    
-    $accountType.change(function() {
-      
-      var $form = $(this).closest('form');
-      
-      var data = {
-        registration:  {
-          accountType: $('#registration_accountType input:checked').val()
-        }
-      };
-     
-      $.ajax({
-        url : $form.attr('action'),
-        type: $form.attr('method'),
-        data : data,
-        success: function(html) {
-          $('#accoutTypeForm').replaceWith(
-            $(html).find('#accoutTypeForm')
-          );
-        }
-      });
-    });
-});
---
+++ assets/js/register-form.js
@@ -0,0 +1,27 @@
+var $ = require('jquery');
+
+$( document ).ready(function() {
+  var $accountType = $('#registration_accountType');
+    
+  $accountType.change(function() {
+      
+    var $form = $(this).closest('form');
+      
+    var data = {
+      registration:  {
+        accountType: $('#registration_accountType input:checked').val()
+      }
+    };
+     
+    $.ajax({
+      url : $form.attr('action'),
+      type: $form.attr('method'),
+      data : data,
+      success: function(html) {
+        $('#accoutTypeForm').replaceWith(
+          $(html).find('#accoutTypeForm')
+        );
+      }
+    });
+  });
+});
