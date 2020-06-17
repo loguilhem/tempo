7d0d367e0daea19af0974798393528b60a334cb1
ericspichiger <e.spichiger@hiveweb.ch>
2020-06-17 18:01:19 +0200

    [UI/UX] Add navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 import '../scss/app.scss';
-
-
+import 'bootstrap';
+import 'material-icons';
 //import $ from 'jquery';
+
+import './navigation-left'
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
