7ecbbaa5f1a9e55dcf8de958b948ef1bafe775a4
Christophe Espiau <christophe.espiau@gmail.com>
2020-07-31 14:57:37 +0200

    Language selection menu added.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -22,4 +22,20 @@ class MainController extends AbstractController
             return $this->render('page/index.html.twig');
         }
     }
+
+    /**
+     * @Route("/change_locale/{locale}", name="change_locale")
+     *
+     * @param $locale
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function changeLocale($locale, Request $request)
+    {
+        // Save locale in session
+        $request->getSession()->set('_locale', $locale);
+
+        // Back to visited page
+        return $this->redirect($request->headers->get('referer'));
+    }
 }
